e6baa0fd29ebf60f71a823fe4521411bcbb9a8d3
emanmkhareez <emkhareez19@gmail.com>
2021-05-20 11:50:15 -0700

    addlab1

---
+++ src/component/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+
+ class Footer extends React.Component{
+     render()
+     {
+         return(
+             <div>
+                 <h2>
+                 author:Eman Mkhareez  
+                 </h2>
+             </div>
+         )
+     }
+
+ }
+ export default Footer;
