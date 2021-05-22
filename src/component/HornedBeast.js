3ed70cf8320ffcf1f5285325a252577e7d9eff01
emanmkhareez <emkhareez19@gmail.com>
2021-05-21 21:47:35 -0700

    add lab4

--- src/component/HornedBeast.js
+++ src/component/HornedBeast.js
@@ -8,28 +8,28 @@ class HornedBeast extends React.Component{
     prop3Function=(para1,para2,para3)=>{
         this.props.prop2Function(para1,para2,para3);
         
-    }
+    };
 
      constructor(props){
          super(props);
          this.state={
              vote:0
-         }
+         };
 
-     }
+     };
 increaseVote =() =>{
     this.setState({
         vote:this.state.vote+1
     })
     
-}
+};
 
     render(){
 
         return(
 
-<div id="div1">
-<Card style={{ width: '18rem' }} onClick={this.prop3Function}>
+<div id="div1"  style ={{display: 'inline-block'}}>
+<Card style={{ width: '18rem' }} onClick={this.prop3Function} >
 <Card.Body>
     <Card.Title>{this.props.title}</Card.Title>
     <Card.Text>
@@ -68,10 +68,10 @@ increaseVote =() =>{
    
    <button onClick={this.increaseVote}>Vote </button>
    <h2>{this.state.vote}</h2>
-         */}
+         */};
 
             </div>
         )
-    }
-}
+    };
+};
 export default HornedBeast;
